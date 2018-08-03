ee8b7a4dfece5c7a35963a9c52e82bcd6f495cbb
JEONGHYUNUK <hyunuk8988@gmail.com>
2018-08-03 10:47:26 +0900

    모바일 로봇 플젝 1

--- yongsang/ctrl_ultrapwm.c
+++ yongsang/ctrl_ultrapwm.c
@@ -5,6 +5,9 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<fcntl.h>
+#include<pthread.h>
+#define DO_L 26
+#define DO_R 19
 #define INPUT1 5
 #define INPUT2 6
 #define INPUT3 17
@@ -16,21 +19,18 @@
 #define TRIG_PINNO3 2
 #define ECHO_PINNO3 3
 
+void *thr_fn1(void *arg);
 void trigger(void);
 void auto_ctrl(int pi, int range);
 void manual_ctrl(int pi,int range);
 void setgpiomode(int pi,int range);
+uint32_t start_encor1, dist_encor1, start_encor2, dist_encor2;
 int left_pwm(int pi,int range);
 int right_pwm(int pi,int range);
 int go_pwm(int pi,int range);
 int back_pwm(int pi,int range);
 int stop_pwm(int pi);
 int go_ctrl(int pi,int range);
-void init_keyboard();
-void close_keyboard();
-int _kbhit();
-int _getch();
-int _putch();
 
 void cb_func_encor1(int pi, unsigned gpio, unsigned level, uint32_t tick);
 void cb_func_encor2(int pi, unsigned gpio, unsigned level, uint32_t tick);
@@ -42,13 +42,11 @@ uint32_t start_tick_1, dist_tick_1;
 uint32_t start_tick_2, dist_tick_2;
 uint32_t start_tick_3, dist_tick_3;
 
-
-static struct termios initial_settings, new_settings;
-static int peek_character = -1;
-
+int key;
 int wgt1=753; //wgt for Right
 int wgt2=750; //wgt for Left
-int range=4000;
+int range=8000;
+int num3 = 0, num4 = 0;
 
 int main(void)
 {
@@ -59,7 +57,6 @@ int main(void)
     newtio.c_lflag &= ~ECHO; // ECHO 입력문자 반향
     tcsetattr(0, TCSANOW, &newtio);//TCSANOW 즉시 속성 변경
      
-
     int pi, num2=0;
     if((pi = pigpio_start(NULL, NULL)) < 0)
     {
@@ -92,8 +89,10 @@ int main(void)
 
 void auto_ctrl(int pi, int range)
 {
+    int err;
+    pthread_t tid1;
+    void *tret;
     float distance1, distance2, distance3;
-    init_keyboard();
     setgpiomode(pi,range);
     int SNR_D = 50;
     while(1)
@@ -147,49 +146,67 @@ void auto_ctrl(int pi, int range)
 void manual_ctrl(int pi,int range)
 {
     int c;
+    int err;
+    pthread_t tid1;
+    void *tret;
     setgpiomode(pi,range);
-    while((c = getc(stdin)) != 97){
-        switch(c)
-        {
-            case 32:
-                {
-                    printf("<STOP>\n");
-                    stop_pwm(pi);
-                    break;
-                }
-            case 27:
-                c = getc(stdin);
-                if(c == 91) {
-                    c = getc(stdin);
-                    if(c == 67)
-                    {
-                        printf("<RIGHT>\n");
-                        right_pwm(pi,range);
-                    }
-                    else if(c == 68)
-                    {
-                        printf("<LEFT>\n");
-                        left_pwm(pi,range);
-                    }
-                    else if(c == 66)
-                    {
-                        printf("<BACK>\n");
-                        back_pwm(pi,range);
-                    }
-                    else if(c == 65)
-                    {
-                        printf("<GO>\n");
-                        go_pwm(pi,range);
-                    }
-                    else
-                        stop_pwm(pi);
-                }
-                break;
-        }
-    }
-    if(ferror(stdin))
+    while(1)
     {
-        printf("Input error\n");
+        err = pthread_create(&tid1, NULL, thr_fn1, NULL);
+        if(err != 0)
+        {
+            exit(1);
+        }
+        err = pthread_join(tid1, &tret);
+        if(err != 0)
+        {
+            exit(1);
+        }
+        printf("manual\n");
+        while((c = getc(stdin)) != 97){
+            switch(c)
+            {
+                case 32:
+                    {
+                        printf("<STOP>\n");
+                        stop_pwm(pi);
+                        break;
+                    }
+                case 27:
+                    c = getc(stdin);
+                    if(c == 91) {
+                        c = getc(stdin);
+                        if(c == 67)
+                        {
+                            printf("<RIGHT>\n");
+                            right_pwm(pi,range);
+                        }
+                        else if(c == 68)
+                        {
+                            printf("<LEFT>\n");
+                            left_pwm(pi,range);
+                        }
+                        else if(c == 66)
+                        {
+                            printf("<BACK>\n");
+                            back_pwm(pi,range);
+                            key = 2;
+                        }
+                        else if(c == 65)
+                        {
+                            printf("<GO>\n");
+                            key = 1;
+                        }
+                        else
+                            stop_pwm(pi);
+                    }
+                    break;
+            }
+        }
+        if(ferror(stdin))
+        {
+            printf("Input error\n");
+        }
     }
 }
 
@@ -272,65 +289,13 @@ int go_pwm(int pi,int range)
     return 1;
 }
 
-void init_keyboard()
-{
-    tcgetattr(0,&initial_settings);
-    new_settings = initial_settings;
-    new_settings.c_lflag &= ~ICANON;
-    new_settings.c_lflag &= ~ECHO;
-    new_settings.c_cc[VMIN] = 1;
-    new_settings.c_cc[VTIME] = 0;
-    tcsetattr(0, TCSANOW, &new_settings);
-}
-
-void close_keyboard()
-{
-    tcsetattr(0, TCSANOW, &initial_settings);
-}
-
-int _kbhit()
-{
-    unsigned char ch;
-    int nread;
-    if (peek_character != -1) return 1;
-    new_settings.c_cc[VMIN]=0;
-    tcsetattr(0, TCSANOW, &new_settings);
-    new_settings.c_cc[VMIN]=1;
-    tcsetattr(0, TCSANOW, &new_settings);
-    if(read(0,&ch,1) == 1) 
-    {
-        peek_character = ch;
-        return 1;
-    }
-    return 0;
-}
-
-int _getch()
-{
-    char ch;
-    if(peek_character != -1) 
-    {
-        ch = peek_character;
-        peek_character = -1;
-        return ch;
-    }
-    read(0,&ch,1);
-    return ch;
-}
-
-int _putch(int c) {
-    putchar(c);
-    fflush(stdout);
-    return c;
-}
-
-
 void cb_func_encor1(int pi, unsigned gpio, unsigned level, uint32_t tick)
 {
     if(level == PI_HIGH)
         start_encor1 = tick;
     else if(level == PI_LOW)
         dist_encor1 = tick - start_encor1;
+    num3++;
 }
 
 void cb_func_encor2(int pi, unsigned gpio, unsigned level, uint32_t tick)
@@ -339,5 +304,60 @@ void cb_func_encor2(int pi, unsigned gpio, unsigned level, uint32_t tick)
         start_encor2 = tick;
     else if(level == PI_LOW)
         dist_encor2 = tick - start_encor2;
+    num4++;
 }
 
+void *thr_fn1(void *arg)
+{
+    int pi = 1;
+    
+    if((pi = pigpio_start(NULL, NULL)) < 0)
+    {
+        fprintf(stderr, "pigpio_start error\n");
+    }
+    printf("실행중\n");
+    setgpiomode(pi, range);
+    set_mode(pi, DO_L, PI_INPUT);
+    set_mode(pi, DO_R, PI_INPUT);
+    callback(pi, DO_L, EITHER_EDGE, cb_func_encor1);
+    callback(pi, DO_R, EITHER_EDGE, cb_func_encor2);
+    while(1){
+    printf("thread running\n");
+    while(key == 1)
+    {
+        go_pwm(pi,range);
+
+        printf("left count : %d, right count : %d\n",num3, num4);
+
+        while(40 < num3)
+        {
+            gpio_write(pi, INPUT1, PI_LOW);
+            gpio_write(pi, INPUT4, PI_LOW);
+            set_PWM_dutycycle(pi, INPUT2, (range*wgt2/1000));
+            gpio_write(pi, INPUT3, PI_LOW);
+            if(41 < num4)
+            {
+                num3 = 1;
+                num4 = 0;
+                break;
+            }
+        }
+        while(40 < num4)
+        {
+            gpio_write(pi, INPUT1, PI_LOW);
+            gpio_write(pi, INPUT4, PI_LOW);
+            set_PWM_dutycycle(pi, INPUT3, (range*wgt1/1000));
+            gpio_write(pi, INPUT2, PI_LOW);
+            if(41 < num3)
+            {
+                num3 = 0;
+                num4 = 1;
+                break;
+            }
+        }
+        if(key != 1)
+            break;
+    }
+    }
+
+}
